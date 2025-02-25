05f20e67db3e8a3a27dddc3e0f803e5099bbf12c
Corono <jimmy.tobing@gmail.com>
2025-02-25 19:08:47 +0700

    JimPuml

--- JimPuml.js
+++ JimPuml.js
@@ -15,14 +15,8 @@ async function main(xmlFilePath, outputJson, outputPuml) {
         const jsonData = await parseSfMeta(xmlData); //convert to json
         const flow = jsonData.Flow; // ambil root Flow
 
-        //Buat file JSON
-        //const jsonString = JSON.stringify(flow, null, '\t');
-        //fs.writeFile(outputJson, jsonString);
-        //console.log(`Json berhasil dibuat: ${outputJson}`);
-
         //Buat file PUML
         const strPuml = generatePUML(flow);
-        //console.log(`PUML: \n${strPuml}`);
         fs.writeFile(outputPuml, strPuml);
         console.log(`Puml berhasil dibuat: ${outputPuml}`);
     } catch (err) {
@@ -32,93 +26,149 @@ async function main(xmlFilePath, outputJson, outputPuml) {
 
 //----------------------------------------------------------------------------------
 
-let objElements = {};
-let arrPuml = pumlStart();
-
+const objElements = {};
 function generatePUML(objFlow){
     let retval = ''; 
 
-    //ekstrak object yang punya Name
-    findElementsWithName(objFlow);
-    objElements['start'] = { type: 'start', data:objFlow['start']  };
+    //ekstrak object yang punya Name kedalam "objElements"
+    objElements['start'] = {type:'start', next: null, zdata: objFlow['start']};
+    findElementsWithName(objFlow,'');
     Object.entries(objElements).forEach(([strKey, objValue]) => {
         console.log(`Debug: ${strKey} (Type: ${objValue.type})`);
-    });   
+        const strNext = cariNext(objValue.type, objValue.zdata);
+        objValue.next = strNext;
+    });
+
 
     //proses sesuai alur
-    let strNext = 'start';
-    while(strNext){
-        strNext = generatePumlLine(strNext);
+    let arrPuml = pumlStart();
+    let strKey = 'start';
+    while(strKey){
+        arrPuml.push(generatePumlLine(strKey));
+        strKey = objElements[strKey].next;
     }
 
-    //Object.keys(objFlow).forEach(key => {retval += `${key} |`;});
     arrPuml.push("stop", "@enduml");
     retval = arrPuml.join('\n');
     return retval;
 }
 
-function generatePumlLine(strNext){
-    let objCur = objElements[strNext];
-    let strBefore = strNext;
+function generatePumlLine(strKey){
+    let retval = '';
+    const objCur = objElements[strKey];
+
+    // 01.decisions
+    if(objCur.type === 'decisions'){
+        const arrTmp = [];
+        arrTmp.push(`switch (${strKey})`);
+            //-------------------------------------
+            const arrRules = Array.isArray(objCur.zdata.rules)? objCur.zdata.rules : [objCur.zdata.rules];
+            arrRules.forEach(rule => {
+                arrTmp.push(`case (${rule.name})`);
+                let strTmp = rule.name;
+                while(strTmp!==objCur.next){
+                    const objTmp = objElements[strTmp];
+                    arrTmp.push(`\t`+generatePumlLine(strTmp));
+                    strTmp = objTmp.next;
+                }
+            });
+            //-------------------------------------
+                arrTmp.push(`case (${objCur.zdata.defaultConnectorLabel})`);
+                    let strElse = objCur.zdata.defaultConnectorLabel;
+                    let strTmp = objCur.zdata.defaultConnector.targetReference;
+                    arrTmp.push(`\t:${strElse}('rules')  --> ${strTmp};<<kondisi>>`);
+
+                while(strTmp!==objCur.next){
+                    const objTmp = objElements[strTmp];
+                    arrTmp.push(`\t`+generatePumlLine(strTmp));
+                    strTmp = objTmp.next;
+                }
+            //-------------------------------------
+        arrTmp.push(`endswitch`);
+        retval = arrTmp.join('\n');
+    }
+
+    // 02.loop
+    else if(objCur.type === 'loop'){
+        
+    }
     
-    if(objCur.type==='decisions'){
-        arrPuml.push(`switch (${strBefore})`);
-        strNext = objCur.data.defaultConnector?.targetReference;
-
-        //recursive
-        arrRules = Array.isArray(objCur.data.rules)? objCur.data.rules : [objCur.data.rules];
-        arrRules.forEach(rule => {
-            arrPuml.push(`case (${rule.name})`);
-            let strNext2 = rule.name;
-            while(strNext2){
-                strNext2 = generatePumlLine(strNext2);
-            }
-        });
-
-        //default decision
-            arrPuml.push(`case (${objCur.data.defaultConnectorLabel})`);
-            let strNext3 = objCur.data.defaultConnectorLabel;
-            let strNext4 = objCur.data.defaultConnector.targetReference;
-            objElements[strNext3] = {name:strNext3, type: 'rules', data: {connector:{targetReference:strNext4}} };
-            objElements[strBefore] = null;
-            while(strNext3){
-                strNext3 = generatePumlLine(strNext3);
-            }
-
-        arrPuml.push(`endswitch`);
-    }else{
-        arrPuml.push(`:${strBefore}(${objCur.type})  --> ${objCur.next};`);
-        strNext = objCur.data.connector?.targetReference;
+    // 03.other
+    else{
+        retval = `:${strKey}(${objCur.type})  --> ${objCur.next};`;
+        if(objCur.type==='rules') retval += '<<kondisi>>';
     }
+    
+    return retval;
+}
 
-    if(objCur['next']){
-        console.log(`Sudah ada ${objCur['next']}`);
-    }else{
-        objCur['next'] = (strNext)?strNext:'end';
-    }
-    //console.log(`processed: ${strBefore}(${objCur.type}) --> ${objCur.next}`);
-    return strNext;
+function findEndIF(arrAnak){
+    let retval='';
+    const objDone={};
+    arrAnak.forEach(anak => {
+        let strNext = anak.next;
+        while(strNext){
+            if(objDone[strNext]) {
+                retval = strNext;
+                return retval;
+            }else{
+                objDone[strNext]=true;
+            }
+            const objData = objElements[strNext].zdata;
+            strNext = objData.connector?.targetReference; //normal
+            strNext = (strNext)? strNext : objData.noMoreValuesConnector?.targetReference;  // loop
+        }
+
+    });
+    return retval;
 }
 
 function findElementsWithName(objData, strParentKey = '') {
+    if(strParentKey.endsWith('processMetadataValues') || strParentKey.endsWith('inputParameters')) return;
     if (objData && typeof objData === 'object') {
         if (objData.name) {
-            objElements[objData.name] = {name:objData.name, type: strParentKey, data: objData };
+            objElements[objData.name] = {name:objData.name, type: strParentKey, next: null, zdata: objData };
         }
         Object.entries(objData).forEach(([strKey, objValue]) => {
-            // Jika objValue adalah array atau object, lanjutkan rekursi dengan strKey sebagai strParentKey
+
+            // Jika objValue adalah array, lanjutkan rekursi dengan strKey sebagai strParentKey
             if (Array.isArray(objValue)) {
                 objValue.forEach(objItem => findElementsWithName(objItem, strKey));
+            
+            // Jika objValue adalah object, lanjutkan rekursi dengan strKey sebagai strParentKey
             } else if (typeof objValue === 'object') {
                 findElementsWithName(objValue, strKey);
             }
+
         });
     }
 }
 
+function cariNext(strParentKey,objData){
+    let strNext;
 
-  
+    // 01.decisions
+    if(strParentKey==='decisions'){
+        const arrAnak = [{name : objData.defaultConnectorLabel, next: objData.defaultConnector.targetReference}]; 
+        const arrRules = Array.isArray(objData.rules)? objData.rules : [objData.rules];
+              arrRules.forEach(rule => {
+                arrAnak.push({name:rule.name, next:rule.connector?.targetReference});
+              });
+        strNext = findEndIF(arrAnak);
+    }
 
+    // 02.loop
+    else if(strParentKey==='loop'){
+        strNext = (strNext)? strNext : objData.noMoreValuesConnector?.targetReference; 
+    }
+
+    // 03.start, assignments, recordLookups, recordUpdates, transforms, rules
+    else{
+        strNext = objData.connector?.targetReference; 
+    }
+
+    return strNext;
+}
 
 function pumlStart(){
     const arrPuml = [
